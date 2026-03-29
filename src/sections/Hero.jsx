032fc58b37c5fc196f406d1b873eb47db758bd1e
Ibrahim Nuhu <ibrahimnuhu0@gmail.com>
2026-03-29 11:21:25 +0100

    feat: implement feature Cards

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -27,13 +27,13 @@ const Hero = () => {
       <header className='flex flex-col justify-center md:w-full md:px-20 px-5'>
        <div className="flex flex-col gap-7">
         <div className="hero-text">
-          <h1>Turning 
+          <h1>Turning
             <span className="slide">
               <span className="wrapper">
                 {words.map((word) => (
                   <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
                     <img
-                     src={word.imgPath} 
+                     src={word.imgPath}
                      alt={word.text}
                      className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
                       />
